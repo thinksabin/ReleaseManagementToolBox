02c80023199aafe5574303f71d672c7f2113d4d6
Sabin Ranjit <sabin.ranjit@logpoint.com>
2019-03-03 16:36:04 +0545

    Updated readme, added build changes checker

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ReleaseManagementToolBox
-Collection of scripts to help to review build artifacts through git. 
+Collection of scripts to help to review build artifacts through git. Can be integrated into CI like Jenkins.
+Useful for the DevOps, Development Team, QA, Release Engineer, etc.
+
+Usage for Build changes checker
+python build_changes_checker.py SBX git-tag-1 git-tag-2
+python build_changes_checker.py SBX branch1 branch2
+
+--------------------
+Sample config.ini
+
+[account]
+user = myusername
+password = mypasswd
+
+[SBX]
+project_url = http://bitbucket.mycompany.com:7990/rest/api/1.0/projects/SBX/repos?limit=50
+repos_to_check = ["helloworld"]
+
+--------------------
+
+You can add project id as new Sections eg; SBX 
+API tested on: Bitbucket
+Python 3.7
---
+++ build_changes_checker/build_changes_checker.py
@@ -0,0 +1,113 @@
+_author_ = 'thinksabin'
+
+
+
+import requests
+import os
+import subprocess
+import sys
+import configparser
+
+from os import path
+
+"""
+Sample Usage:
+python build_changes_checker.py SBX git-tag-1 git-tag-2
+python build_changes_checker.py SBX branch1 branch2
+"""
+
+
+
+def write_to_file(base_path, base_patch_name, latest_patch_name, reponame):
+    with open(base_path +'/' + commits_filename, 'a') as commits_f:
+
+        git_diff = subprocess.Popen(["git", "diff" ,  base_patch_name, latest_patch_name, "--name-only"], stdout=subprocess.PIPE)
+        output = git_diff.communicate('')[0]
+
+        if len(output)!=0:
+
+            cf = subprocess.Popen(["git", "log" , '--pretty=format:"%h; subject:%s; author: %cn"', base_patch_name + ".." + latest_patch_name], stdout=subprocess.PIPE)
+            commits_output = cf.communicate()
+
+            commits_f.write("============================================================================="+ "\n")
+            commits_f.write("Repo : " + str(reponame) + "\n")
+            for commits in commits_output:
+
+                commits_f.write(str(commits))
+                commits_f.write("\n")
+        else:
+            print('Could not find proper output from git diff')
+
+
+def delete_and_create_file(commits_filename):
+    file_header = 'Comparing:   : ' + base_patch_name + ' VS ' + latest_patch_name
+
+    if path.exists(commits_filename):
+        os.remove(commits_filename)
+        print('Old file ', commits_filename, ' removed')
+
+    with open(commits_filename, 'a+') as commits_f:
+        commits_f.write(file_header)
+        commits_f.write("\n")
+
+
+def do_bitbucket_api_call(project_url, repos_to_check):
+
+
+    api_req = requests.get(project_url, auth=(user, password))
+    list_of_repos = api_req.json()["values"]
+    base_path = os.getcwd()
+
+    for repo in list_of_repos:
+        if repo["slug"] in repos_to_check:
+            print(repo["slug"])
+
+            if os.path.exists(repo["slug"]):
+                os.chdir(base_path + "/" + repo["slug"])
+                p = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+                p.communicate()
+                write_to_file(base_path, base_patch_name, latest_patch_name, repo["slug"])
+                os.chdir(base_path)
+            else:
+                clone_url = repo["links"]["clone"]
+                final_url = ""
+                for urls in clone_url:
+                    if urls["name"] == "ssh":
+                        final_url = urls["href"]
+                p = subprocess.Popen(["git", "clone", final_url ], stdout=subprocess.PIPE)
+                p.communicate()
+                os.chdir(base_path + "/" + repo["slug"])
+                p = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+                p.communicate()
+
+                write_to_file(base_path, base_patch_name, latest_patch_name, repo["slug"])
+                os.chdir(base_path)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+
+        project_id = sys.argv[1].strip()
+        base_patch_name = sys.argv[2].strip()
+        latest_patch_name = sys.argv[3].strip()
+
+        try:
+            config = configparser.ConfigParser()
+            config.read('config.ini')
+
+            user = config['account']['user']
+            password = config['account']['password']
+
+            project_url = config[project_id]["project_url"]
+            repos_to_check = config[project_id]["repos_to_check"]
+            commits_filename = base_patch_name + '_' + latest_patch_name + '.commits'
+
+            delete_and_create_file(commits_filename)
+            do_bitbucket_api_call(project_url, repos_to_check)
+        except Exception as e:
+            print('Couldnt find config.ini file. ')
+    else:
+        print('Requires 4 arguements like in example')
+        print('Sample Usage: python build_changes_checker.py SBX git-tag-1 git-tag-2')
+
